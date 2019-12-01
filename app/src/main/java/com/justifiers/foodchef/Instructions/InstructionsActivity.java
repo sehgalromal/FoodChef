414f39de4ae84469321be65913ebe89a201d24bf
Mykhailo Herasimov <mogerasimov@gmail.com>
2019-12-01 17:15:42 -0400

    created layout for recipe instructions

---
+++ app/src/main/java/com/justifiers/foodchef/Instructions/InstructionsActivity.java
@@ -0,0 +1,16 @@
+package com.justifiers.foodchef.Instructions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.justifiers.foodchef.R;
+
+public class InstructionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions);
+    }
+}
