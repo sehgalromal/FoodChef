7cb64d72bedf846191475af11199fa882fe72a15
Mykhailo Herasimov <mogerasimov@gmail.com>
2019-12-02 00:23:24 -0400

    added logic for closing an activity

--- app/src/main/java/com/justifiers/foodchef/Instructions/InstructionsActivity.java
+++ app/src/main/java/com/justifiers/foodchef/Instructions/InstructionsActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.justifiers.foodchef.R;
@@ -24,6 +25,8 @@ public class InstructionsActivity extends AppCompatActivity {
 
     private Button btnNextStep;
     private Button btnPreviousStep;
+    private ImageButton btnClose;
+
     private TextView txtStepNumber;
     private ViewPager stepPager;
     private InstructionsViewPagerAdapter instructionsViewPagerAdapter;
@@ -57,14 +60,27 @@ public class InstructionsActivity extends AppCompatActivity {
 
     protected void configureNavigationButtons() {
 
+        btnClose = findViewById(R.id.btn_instr_close);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeActivity();
+            }
+        });
+
         btnNextStep = findViewById(R.id.btn_instr_next);
         btnNextStep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                int position = stepPager.getCurrentItem();
+                int position = stepPager.getCurrentItem() + 1;
+                Log.d(TAG, "onClick: position = " + position + " size = " + steps.size());
                 if (position < steps.size()) {
-                    stepPager.setCurrentItem(position + 1);
+                    stepPager.setCurrentItem(position);
+                }
+
+                if (position == steps.size()) {
+                    closeActivity();
                 }
             }
         });
@@ -109,7 +125,6 @@ public class InstructionsActivity extends AppCompatActivity {
         stepPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//                Log.d(TAG, "onPageScrolled: changed to " + position);
             }
 
             @Override
@@ -139,7 +154,6 @@ public class InstructionsActivity extends AppCompatActivity {
 
             @Override
             public void onPageScrollStateChanged(int state) {
-//                Log.d(TAG, "onPageScrollStateChanged: " + state);
             }
         });
     }
@@ -150,14 +164,19 @@ public class InstructionsActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // uncomment this line if there will be navigation bar at the top
-//        getSupportActionBar().hide();
+        // getSupportActionBar().hide();
+    }
+
+    protected void closeActivity() {
+        Log.d(TAG, "closeActivity: close activity");
+        finish();
     }
 
     protected PositionState getMovingState(int newPosition) {
 
         int lastPosition = position;
         int maxPosition = steps.size() - 1;
-        Log.d(TAG, "getMovingState: lastPosition = " + lastPosition + " maxPosition " + maxPosition + "new position " + newPosition);
+        Log.d(TAG, "getMovingState: lastPosition = " + lastPosition + " maxPosition " + maxPosition + " new position " + newPosition);
         if (lastPosition == 1 && newPosition == 0) {
             return PositionState.TO_FIRST;
         }
